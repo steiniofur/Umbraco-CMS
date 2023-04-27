8f01de582d9b638b71487b45f9d4e6766f7c9c61
Zeegaan <nge@umbraco.dk>
2023-04-27 13:40:31 +0200

    add playwright API tests

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/Api/languages.spec.ts
@@ -0,0 +1,9 @@
+import {test, expect} from '@playwright/test';
+
+test.describe('Test for AllowEditInvariantFromNonDefault=False', () => {
+  const baseUrl = 'http://localhost:44331';
+
+  test('Can create language', async ({request}) => {
+
+  });
+});
