b46b421c38453a6019a4d43531b2348a7f98b80a
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-11-13 18:55:15 +1300

    fix divider

--- src/Umbraco.Web.UI.Login/src/components/layouts/external-login-providers-layout.element.ts
+++ src/Umbraco.Web.UI.Login/src/components/layouts/external-login-providers-layout.element.ts
@@ -15,7 +15,13 @@ export class UmbExternalLoginProvidersLayoutElement extends LitElement {
 
 	render() {
 		return html`
-			${this.showDivider ? html` <div id="divider" aria-hidden="true"><span><umb-localize key="general_or">Or</umb-localize></span></div> ` : nothing}
+			${this.showDivider
+				? html`
+						<div id="divider" aria-hidden="true">
+							<span><umb-localize key="general_or">Or</umb-localize></span>
+						</div>
+				  `
+				: nothing}
 			<div>
 				<slot></slot>
 			</div>
@@ -44,27 +50,31 @@ export class UmbExternalLoginProvidersLayoutElement extends LitElement {
 				width: 100%;
 				text-align: center;
 				color: var(--uui-color-interactive);
-				position: relative;
 				z-index: 0;
 				margin-bottom: 16px;
+				overflow: hidden;
 			}
 
 			#divider span {
-				background-color: var(--uui-color-surface-alt);
-				padding: 0 9px;
-        text-transform: capitalize;
+				padding-inline: 10px;
+				position: relative;
 			}
 
-			#divider::before {
+			#divider span::before,
+			#divider span::after {
 				content: '';
 				display: block;
-				width: 100%;
+				width: 500px;
 				height: 1px;
 				background-color: var(--uui-color-border);
 				position: absolute;
 				top: calc(50% + 1px);
-				transform: translateY(-50%);
-				z-index: -1;
+			}
+			#divider span::before {
+				right: 100%;
+			}
+			#divider span::after {
+				left: 100%;
 			}
 		`,
 	];
