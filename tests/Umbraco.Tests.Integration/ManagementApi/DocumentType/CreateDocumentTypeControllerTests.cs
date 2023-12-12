f9ad70149732bacfd6c1855d0cfd736f6cd8c4df
Andreas Zerbst <andr317c@live.dk>
2023-12-12 20:05:25 +0100

    Added integration test for document types

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/DocumentType/CreateDocumentTypeControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http.Json;
+using Umbraco.Cms.Api.Management.Controllers.DocumentType;
+using Umbraco.Cms.Api.Management.ViewModels.Document;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.DocumentType;
+
+public class CreateDocumentTypeControllerTests : ManagementApiUserGroupTestBase<CreateDocumentTypeController>
+{
+    protected override Expression<Func<CreateDocumentTypeController, object>> MethodSelector =>
+        x => x.Create(null);
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.BadRequest
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden,
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+
+    protected override async Task<HttpResponseMessage> ClientRequest()
+    {
+        CreateDocumentRequestModel createDocumentRequestModel = new()
+        {
+            ContentTypeId = Guid.NewGuid(),
+            TemplateId = null,
+        };
+
+        return await Client.PostAsync(Url, JsonContent.Create(createDocumentRequestModel));
+    }
+}
---
+++ tests/Umbraco.Tests.Integration/ManagementApi/DocumentType/Item/ItemDocumentTypeItemControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using Umbraco.Cms.Api.Management.Controllers.DocumentType.Item;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.DocumentType.Item;
+
+public class ItemDocumentTypeItemControllerTests : ManagementApiUserGroupTestBase<ItemDocumentTypeItemController>
+{
+    protected override Expression<Func<ItemDocumentTypeItemController, object>> MethodSelector =>
+        x => x.Item(new HashSet<Guid> { Guid.NewGuid() });
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden,
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+}
