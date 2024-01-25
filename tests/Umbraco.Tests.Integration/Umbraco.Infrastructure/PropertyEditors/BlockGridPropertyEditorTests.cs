e0ec3a6525df1a1ea626d12cbfb3fdc22943b42f
Andreas Zerbst <andr317c@live.dk>
2024-01-25 21:54:29 +0100

    Start of integration test

---
+++ tests/Umbraco.Tests.Integration/Umbraco.Infrastructure/PropertyEditors/BlockGridPropertyEditorTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Tests.Common.Builders;
+using Umbraco.Cms.Tests.Common.Testing;
+using Umbraco.Cms.Tests.Integration.Testing;
+
+namespace Umbraco.Cms.Tests.Integration.Umbraco.Infrastructure.PropertyEditors;
+
+// Test Tags and Reference Tracking
+
+[TestFixture]
+[UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerTest)]
+public class BlockGridPropertyEditorTests : UmbracoIntegrationTest
+{
+    private IDataTypeService DataTypeService => GetRequiredService<IDataTypeService>();
+
+    private IContentTypeService ContentTypeService => GetRequiredService<IContentTypeService>();
+
+    private IContentService ContentService => GetRequiredService<IContentService>();
+
+    [Test]
+    public void Can_Track_Block_References()
+    {
+        var blockGridDataType = DataTypeBuilder
+
+        var contentType = ContentTypeBuilder.CreateTextPageContentType("myContentType");
+        contentType.AllowedTemplates = Enumerable.Empty<ITemplate>();
+        ContentTypeService.Save(contentType);
+
+        var dataType = DataTypeService.GetDataType(contentType.PropertyTypes.First(propertyType => propertyType.Alias == "bodyText").DataTypeId)!;
+        var editor = dataType.Editor!;
+        var valueEditor = editor.GetValueEditor();
+
+        const string markup = "<p>This is some markup</p>";
+
+        var content = ContentBuilder.CreateTextpageContent(contentType, "My Content", -1);
+        content.Properties["bodyText"]!.SetValue(markup);
+        ContentService.Save(content);
+
+        var toEditor = valueEditor.ToEditor(content.Properties["bodyText"]);
+        var richTextEditorValue = toEditor as RichTextEditorValue;
+
+        Assert.IsNotNull(richTextEditorValue);
+        Assert.AreEqual(markup, richTextEditorValue.Markup);
+    }
+}
+
---
+++ tests/Umbraco.Tests.Integration/Umbraco.Infrastructure/PropertyEditors/BlockListPropertyEditorTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Tests.Common.Testing;
+using Umbraco.Cms.Tests.Integration.Testing;
+
+namespace Umbraco.Cms.Tests.Integration.Umbraco.Infrastructure.PropertyEditors;
+
+// Test Tags and Reference Tracking
+
+[TestFixture]
+[UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerTest)]
+public class BlockListPropertyEditorTests : UmbracoIntegrationTest
+{
+
+    private IDataTypeService DataTypeService => GetRequiredService<IDataTypeService>();
+
+    private IContentTypeService ContentTypeService => GetRequiredService<IContentTypeService>();
+
+    private IContentService ContentService => GetRequiredService<IContentService>();
+
+}
