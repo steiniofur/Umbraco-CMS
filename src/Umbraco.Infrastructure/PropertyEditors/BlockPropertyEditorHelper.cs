77ca7ba40353d4c86912795502fbaf482f2c9a3f
Andreas Zerbst <andr317c@live.dk>
2024-02-08 19:10:48 +0100

    Added serializer for Blocks

---
+++ src/Umbraco.Infrastructure/PropertyEditors/BlockPropertyEditorHelper.cs
@@ -0,0 +1,10 @@
+﻿using Umbraco.Cms.Core.Models.Blocks;
+using Umbraco.Cms.Core.Serialization;
+
+namespace Umbraco.Cms.Core.PropertyEditors;
+
+public static class BlockPropertyEditorHelper
+{
+
+    public static string SerializeBlocks(BlockValue blockValue, IJsonSerializer jsonSerializer) => jsonSerializer.Serialize(blockValue);
+}
