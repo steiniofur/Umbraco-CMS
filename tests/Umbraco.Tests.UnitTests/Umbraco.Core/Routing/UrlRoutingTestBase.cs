0daab57fa654122780e0961bfbe0d62d8556d30a
Sven Geusens <sge@umbraco.dk>
2024-08-26 17:18:19 +0200

    Remove XmlContent dependency in favor of contentKits from Routing tests

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Routing/UrlRoutingTestBase.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Routing/UrlRoutingTestBase.cs
@@ -5,6 +5,9 @@ using NUnit.Framework;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Infrastructure.PublishedCache;
+using Umbraco.Cms.Infrastructure.PublishedCache.DataSource;
+using Umbraco.Cms.Tests.Common.Builders;
+using Umbraco.Cms.Tests.Common.Builders.Extensions;
 using Umbraco.Cms.Tests.Common.Published;
 using Umbraco.Cms.Tests.UnitTests.TestHelpers;
 
@@ -17,20 +20,12 @@ public abstract class UrlRoutingTestBase : PublishedSnapshotServiceTestBase
     public override void Setup()
     {
         base.Setup();
-
-        var xml = GetXmlContent(1234);
-
-        IEnumerable<ContentNodeKit> kits = PublishedContentXmlAdapter.GetContentNodeKits(
-            xml,
-            TestHelper.ShortStringHelper,
-            out var contentTypes,
-            out var dataTypes).ToList();
-
-        InitializedCache(kits, contentTypes, dataTypes);
+        PopulateCache();
     }
 
     // Sets up the mock domain service
-    protected override ServiceContext CreateServiceContext(IContentType[] contentTypes, IMediaType[] mediaTypes, IDataType[] dataTypes)
+    protected override ServiceContext CreateServiceContext(IContentType[] contentTypes, IMediaType[] mediaTypes,
+        IDataType[] dataTypes)
     {
         var serviceContext = base.CreateServiceContext(contentTypes, mediaTypes, dataTypes);
 
@@ -56,142 +51,80 @@ public abstract class UrlRoutingTestBase : PublishedSnapshotServiceTestBase
         return serviceContext;
     }
 
-    protected virtual string GetXmlContent(int templateId)
-        => @"<?xml version=""1.0"" encoding=""utf-8""?>
-<!DOCTYPE root[
-<!ELEMENT Doc ANY>
-<!ATTLIST Doc id ID #REQUIRED>
-]>
-<root id=""-1"">
-    <Doc id=""1001"" parentID=""-1"" level=""1"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""0"" createDate=""2012-06-12T14:13:17"" updateDate=""2012-07-20T18:50:43"" nodeName=""Home"" urlName=""1001"" writerName=""admin"" creatorName=""admin"" path=""-1,1001"" isDoc="""">
-        <content><![CDATA[]]></content>
-        <umbracoUrlAlias><![CDATA[this/is/my/alias, anotheralias]]></umbracoUrlAlias>
-        <Doc id=""10011"" parentID=""1001"" level=""2"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""0"" createDate=""2012-07-20T18:06:45"" updateDate=""2012-07-20T19:07:31"" nodeName=""Sub1"" urlName=""1001-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10011"" isDoc="""">
-            <content><![CDATA[<div>This is some content</div>]]></content>
-            <umbracoUrlAlias><![CDATA[page2/alias, 2ndpagealias, en/flux, endanger]]></umbracoUrlAlias>
-            <Doc id=""100111"" parentID=""10011"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""0"" createDate=""2012-07-20T18:07:54"" updateDate=""2012-07-20T19:10:27"" nodeName=""Sub2"" urlName=""1001-1-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10011,100111"" isDoc="""">
-                <content><![CDATA[]]></content>
-                <umbracoUrlAlias><![CDATA[only/one/alias, entropy, bar/foo, en/bar/nil]]></umbracoUrlAlias>
-            </Doc>
-            <Doc id=""100112"" parentID=""10011"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1001-1-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10011,100112"" isDoc="""">
-                <content><![CDATA[]]></content>
-                <Doc id=""1001121"" parentID=""100112"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1001-1-2-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10011,100112,1001121"" isDoc="""">
-                    <content><![CDATA[]]></content>
-                </Doc>
-                <Doc id=""1001122"" parentID=""100112"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1001-1-2-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10011,100112,1001122"" isDoc="""">
-                    <content><![CDATA[]]></content>
-                </Doc>
-            </Doc>
-        </Doc>
-        <Doc id=""10012"" parentID=""1001"" level=""2"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:01"" updateDate=""2012-07-20T18:49:32"" nodeName=""Sub 2"" urlName=""1001-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10012"" isDoc="""">
-            <content><![CDATA[<div>This is some content</div>]]></content>
-            <umbracoUrlAlias><![CDATA[alias42]]></umbracoUrlAlias>
-            <Doc id=""100121"" parentID=""10012"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""0"" createDate=""2012-07-20T18:07:54"" updateDate=""2012-07-20T19:10:27"" nodeName=""Sub2"" urlName=""1001-2-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10012,100121"" isDoc="""">
-                <content><![CDATA[]]></content>
-                <umbracoUrlAlias><![CDATA[alias43]]></umbracoUrlAlias>
-            </Doc>
-            <Doc id=""100122"" parentID=""10012"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1001-2-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10012,100122"" isDoc="""">
-                <content><![CDATA[]]></content>
-                <Doc id=""1001221"" parentID=""100122"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1001-2-2-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10012,100122,1001221"" isDoc="""">
-                    <content><![CDATA[]]></content>
-                </Doc>
-                <Doc id=""1001222"" parentID=""100122"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1001-2-2-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10012,100122,1001222"" isDoc="""">
-                    <content><![CDATA[]]></content>
-                </Doc>
-            </Doc>
-        </Doc>
-        <Doc id=""10013"" parentID=""1001"" level=""2"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""2"" createDate=""2012-07-20T18:08:01"" updateDate=""2012-07-20T18:49:32"" nodeName=""Sub 2"" urlName=""1001-3"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10013"" isDoc="""">
-            <umbracoUrlAlias><![CDATA[alias42]]></umbracoUrlAlias>
-        </Doc>
-    </Doc>
-    <Doc id=""1002"" parentID=""-1"" level=""1"" writerID=""0"" creatorID=""0"" nodeType=""1234"" template=""" +
-           templateId +
-           @""" sortOrder=""2"" createDate=""2012-07-16T15:26:59"" updateDate=""2012-07-18T14:23:35"" nodeName=""Test"" urlName=""1002"" writerName=""admin"" creatorName=""admin"" path=""-1,1002"" isDoc="""">
-    </Doc>
-    <Doc id=""1003"" parentID=""-1"" level=""1"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""0"" createDate=""2012-06-12T14:13:17"" updateDate=""2012-07-20T18:50:43"" nodeName=""Home"" urlName=""1003"" writerName=""admin"" creatorName=""admin"" path=""-1,1003"" isDoc="""">
-        <content><![CDATA[]]></content>
-        <Doc id=""10031"" parentID=""1003"" level=""2"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""0"" createDate=""2012-07-20T18:06:45"" updateDate=""2012-07-20T19:07:31"" nodeName=""Sub1"" urlName=""1003-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10031"" isDoc="""">
-            <content><![CDATA[<div>This is some content</div>]]></content>
-            <Doc id=""100311"" parentID=""10031"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""0"" createDate=""2012-07-20T18:07:54"" updateDate=""2012-07-20T19:10:27"" nodeName=""Sub2"" urlName=""1003-1-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10031,100311"" isDoc="""">
-                <content><![CDATA[]]></content>
-            </Doc>
-            <Doc id=""100312"" parentID=""10031"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1003-1-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10031,100312"" isDoc="""">
-                <content><![CDATA[]]></content>
-                <Doc id=""1003121"" parentID=""100312"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1003-1-2-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10031,100312,1003121"" isDoc="""">
-                    <content><![CDATA[]]></content>
-                </Doc>
-                <Doc id=""1003122"" parentID=""100312"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1003-1-2-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10031,100312,1003122"" isDoc="""">
-                    <content><![CDATA[]]></content>
-                </Doc>
-            </Doc>
-        </Doc>
-        <Doc id=""10032"" parentID=""1003"" level=""2"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:01"" updateDate=""2012-07-20T18:49:32"" nodeName=""Sub 2"" urlName=""1003-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10032"" isDoc="""">
-            <content><![CDATA[<div>This is some content</div>]]></content>
-            <Doc id=""100321"" parentID=""10032"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""0"" createDate=""2012-07-20T18:07:54"" updateDate=""2012-07-20T19:10:27"" nodeName=""Sub2"" urlName=""1003-2-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10032,100321"" isDoc="""">
-                <content><![CDATA[]]></content>
-            </Doc>
-            <Doc id=""100322"" parentID=""10032"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1003-2-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10032,100322"" isDoc="""">
-                <content><![CDATA[]]></content>
-                <Doc id=""1003221"" parentID=""100322"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""0"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1003-2-2-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10032,100322,1003221"" isDoc="""">
-                    <content><![CDATA[]]></content>
-                </Doc>
-                <Doc id=""1003222"" parentID=""100322"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1003-2-2-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10032,100322,1003222"" isDoc="""">
-                    <content><![CDATA[]]></content>
-                </Doc>
-            </Doc>
-        </Doc>
-        <Doc id=""10033"" parentID=""1003"" level=""2"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" +
-           templateId +
-           @""" sortOrder=""2"" createDate=""2012-07-20T18:08:01"" updateDate=""2012-07-20T18:49:32"" nodeName=""Sub 2"" urlName=""1003-3"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10033"" isDoc="""">
-        </Doc>
-    </Doc>
-</root>";
+    protected virtual void PopulateCache()
+    {
+        var dataTypes = GetDefaultDataTypes().Select(dt => dt as IDataType).ToArray();
+        var propertyDataTypes = new Dictionary<string, IDataType>
+        {
+            // we only have one data type for this test which will be resolved with string empty.
+            [string.Empty] = dataTypes[0],
+        };
+        IContentType emptyContentType = new ContentType(ShortStringHelper, -1);
+        emptyContentType.Alias = "alias";
+
+        var kits = new[]
+        {
+            BuildKit(1001, "Home", "1001", "-1,1001", ["this/is/my/alias", "anotheralias"], propertyDataTypes, emptyContentType),
+            BuildKit(10011, "Sub1", "1001-1", "-1,1001,10011", ["page2/alias", "2ndpagealias", "en/flux", "endanger"], propertyDataTypes, emptyContentType),
+            BuildKit(100111, "Sub2", "1001-1-1", "-1,1001,10011,100111", ["only/one/alias", "entropy", "bar/foo", "en/bar/nil"], propertyDataTypes, emptyContentType),
+            BuildKit(100112, "Sub 3", "1001-1-2", "-1,1001,10011,100112", null, propertyDataTypes, emptyContentType),
+            BuildKit(1001121, "Sub 3", "1001-1-2-1", "-1,1001,10011,100112,1001121", null, propertyDataTypes, emptyContentType),
+            BuildKit(1001122, "Sub 3", "1001-1-2-2", "-1,1001,10011,100112,1001122", null, propertyDataTypes, emptyContentType),
+            BuildKit(10012, "Sub 2", "1001-2", "-1,1001,10012", ["alias42"], propertyDataTypes, emptyContentType),
+            BuildKit(100121, "Sub2", "1001-2-1", "-1,1001,10012,100121", ["alias43"], propertyDataTypes, emptyContentType),
+            BuildKit(100122, "Sub 3", "1001-2-2", "-1,1001,10012,100122", null, propertyDataTypes, emptyContentType),
+            BuildKit(1001221, "Sub 3", "1001-2-2-1", "-1,1001,10012,100122,1001221", null, propertyDataTypes, emptyContentType),
+            BuildKit(1001222, "Sub 3", "1001-2-2-2", "-1,1001,10012,100122,1001222", null, propertyDataTypes, emptyContentType),
+            BuildKit(10013, "Sub 2", "1001-3", "-1,1001,10013", ["alias42"], propertyDataTypes, emptyContentType),
+            BuildKit(1002, "Test", "1002", "-1,1002", null, propertyDataTypes, emptyContentType),
+            BuildKit(1003, "Home", "1003", "-1,1003", null, propertyDataTypes, emptyContentType),
+            BuildKit(10031, "Sub1", "1003-1", "-1,1003,10031", null, propertyDataTypes, emptyContentType),
+            BuildKit(100311, "Sub2", "1003-1-1", "-1,1003,10031,100311", null, propertyDataTypes, emptyContentType),
+            BuildKit(100312, "Sub 3", "1003-1-2", "-1,1003,10031,100312", null, propertyDataTypes, emptyContentType),
+            BuildKit(1003121, "Sub 3", "1003-1-2-1", "-1,1003,10031,100312,1003121", null, propertyDataTypes, emptyContentType),
+            BuildKit(1003122, "Sub 3", "1003-1-2-2", "-1,1003,10031,100312,1003122", null, propertyDataTypes, emptyContentType),
+            BuildKit(10032, "Sub 2", "1003-2", "-1,1003,10032", null, propertyDataTypes, emptyContentType),
+            BuildKit(100321, "Sub2", "1003-2-1", "-1,1003,10032,100321", null, propertyDataTypes, emptyContentType),
+            BuildKit(100322, "Sub 3", "1003-2-2", "-1,1003,10032,100322", null, propertyDataTypes, emptyContentType),
+            BuildKit(1003221, "Sub 3", "1003-2-2-1", "-1,1003,10032,100322,1003221", null, propertyDataTypes, emptyContentType),
+            BuildKit(1003222, "Sub 3", "1003-2-2-2", "-1,1003,10032,100322,1003222", null, propertyDataTypes, emptyContentType),
+            BuildKit(10033, "Sub 2", "1003-3", "-1,1003,10033", null, propertyDataTypes, emptyContentType),
+        };
+
+        InitializedCache(kits, [emptyContentType], dataTypes);
+    }
+
+    protected ContentNodeKit BuildKit(
+        int id,
+        string name,
+        string urlName,
+        string path,
+        string[]? urlAliases,
+        Dictionary<string, IDataType> propertyDataTypes,
+        IContentType contentType)
+    {
+        var contentDataBuilder = new ContentDataBuilder()
+            .WithName(name)
+            .WithUrlSegment(urlName);
+
+        if (urlAliases is not null)
+        {
+            contentDataBuilder.WithProperties(new Dictionary<string, PropertyData[]>
+            {
+                { "umbracoUrlAlias", [new PropertyData { Culture = string.Empty, Segment = string.Empty, Value = string.Join(',', urlAliases) }] },
+            });
+        }
+
+        var homeData = contentDataBuilder.Build(ShortStringHelper, propertyDataTypes, contentType, contentType.Alias);
+
+        return ContentNodeKitBuilder.CreateWithContent(
+            contentType.Id,
+            id,
+            path,
+            level: path.Split(',').Length - 1,
+            draftData: homeData,
+            publishedData: homeData);
+    }
 
     public const int LangDeId = 333;
     public const int LangEngId = 334;
