059679b7dbee670d1987472943669a68fc9ddea5
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2024-09-12 10:18:38 +0200

    Re-add runtime compilation to web common

---
+++ src/Umbraco.CMS.ModelsBuilder.InMemoryAuto/UmbracoCompilationException.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace Umbraco.CMS.ModelsBuilder.InMemoryAuto;
+
+internal class UmbracoCompilationException : Exception, ICompilationException
+{
+    public IEnumerable<CompilationFailure?>? CompilationFailures { get; init; }
+}
